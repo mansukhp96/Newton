737e80480a6445d5b446cd3008957c0b8fe950ac
Mansukh <posixnnja@gmail.com>
2019-01-14 14:06:01 -0500

    Final Sub

---
+++ src/SimpleProjectile.java
@@ -0,0 +1,69 @@
+public class SimpleProjectile implements Particle {
+
+  public float x;
+  public float y;
+  public float ay = 9.81f;
+  public float ax = 0f;
+  public float vx;
+  public float vy;
+
+  /**
+   * Constructs a SimpleProjectile object and initializes it to x, y, vx and vy.
+   *
+   * @param x  Axis X Coordinate
+   * @param y  Axis Y Coordinate
+   * @param vx velocity along x-axis
+   * @param vy velocity along y-axis
+   */
+
+  //default constructor taking 4 params
+  public SimpleProjectile(float x, float y, float vx, float vy) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+  }
+
+  @Override
+  public String getState(float time) { //x component and y component displacement
+
+    float timeNew = 0; //to fix the time change in the string
+
+    if (time > 0) { // when time is positive
+      float ny = y + (vy * time) + (0.5f * -ay * (time * time)); // Y- component calc.
+      if (ny <= y) { //After hitting ground to fix x and y
+        timeNew = time;
+        ny = y;
+        time = (2 * vy / ay); // Time to hit the ground.
+        float nx = x + (vx * time) + (0.5f * -ax * (time * time)); // X - when already hit ground.
+      }
+
+      float nx = x + (vx * time) + (0.5f * -ax * (time * time)); // X - component calc.
+
+      if (timeNew != 0) { //print timeNew in string in stop x case
+        String str =
+                String.format(
+                        "At time " + "%.2f" + ": position is (" + "%.2f" + "," + "%.2f" + ")",
+                        timeNew, nx, ny);
+        return str;
+      } else if (timeNew == 0) { //print time from interface in case of all other cases
+        String str =
+                String.format(
+                        "At time " + "%.2f" + ": position is (" + "%.2f" + "," + "%.2f" + ")",
+                        time, nx, ny);
+        return str;
+      }
+    } else if (time <= 0) { //for negative time
+
+      float nx = x;
+      float ny = y;
+
+      String str =
+              String.format(
+                      "At time " + "%.2f" + ": position is (" + "%.2f" + "," + "%.2f" + ")",
+                      time, nx, ny);
+      return str;
+    }
+    return toString();
+  }
+}
---
+++ test/SimpleProjectileTest.java
@@ -0,0 +1,56 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleProjectileTest {
+
+  private SimpleProjectile sp;
+
+  @Before
+  public void setUp() {
+
+    sp = new SimpleProjectile(2.0f, 1.0f, 1.00f, 0);
+
+  }
+
+  @Test
+  public void testTimeOne() {
+    assertEquals("At time 1.00: position is (3.00,6.09)", sp.getState(1f));
+  }
+
+  @Test
+  public void testTimeZero() {
+    assertEquals("At time 0.00: position is (2.00,1.00)", sp.getState(0.00f));
+  }
+
+  @Test
+  public void testTimeNeg() {
+    assertEquals("At time -2.00: position is (2.00,1.00)", sp.getState(-2.0f));
+  }
+
+  @Test
+  public void testTimeMax() {
+    assertEquals("At time 2.04: position is (4.04,1.00)", sp.getState(2.04f));
+  }
+
+  @Test
+  public void testFixedAfterHit() {
+    assertEquals("At time 10.00: position is (4.04,1.00)", sp.getState(10f));
+  }
+
+  @Test
+  public void testNegXMaxTime() {
+    assertEquals("At time 10.00: position is (0.04,1.00)", sp.getState(10f));
+  }
+
+  @Test
+  public void testNegYMaxTime() {
+    assertEquals("At time 10.00: position is (4.04,-1.00)", sp.getState(10f));
+  }
+
+  @Test
+  public void testNegXVelocityMaxTime() {
+    assertEquals("At time 10.00: position is (-0.04,1.00)", sp.getState(10f));
+  }
+}
